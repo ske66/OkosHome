3da60d2b0ca4cd0ba5af3c24ecfeda402c51f569
DESKTOP-7VSRJDF\MarkB <40204953@live.napier.ac.uk>
2018-02-13 18:28:41 +0000

    login and sign up classes started

---
+++ app/src/main/java/uk/ac/napier/okoshome/LoginActivity.java
@@ -0,0 +1,24 @@
+package uk.ac.napier.okoshome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+}
